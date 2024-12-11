b13c8a1f5c85a21310ee7b3ca2b926225c59d86b
Jefferson Llerena <150809369+jeffdanurss@users.noreply.github.com>
2024-12-11 15:08:07 -0500

    feat:

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from models import db
 from schema import schema
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Usamos SQLite para este ejemplo
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+@app.route('/')
+def home():
+    return "¡Hola, mundo!"
 # Crear la base de datos si no existe
 with app.app_context():
     db.create_all()
