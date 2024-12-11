48b56df54eca963928d195653df28af871ae88eb
Jefferson Llerena <150809369+jeffdanurss@users.noreply.github.com>
2024-12-11 15:17:31 -0500

    fix:

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from schema import schema
 
 # Crear una instancia de la aplicación Flask
 app = Flask(__name__)
+# Ruta raíz
+@app.route('/')
+def home():
+    return "Bienvenido a la API GraphQL"
 
 # Configurar la ruta /graphql para manejar las peticiones GraphQL
 app.add_url_rule(
